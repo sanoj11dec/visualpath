d2047ba9ac4c948d9e749f75b349324bd764b0b9
Ubuntu <ubuntu@ip-172-31-28-208.ap-southeast-2.compute.internal>
2024-03-07 01:27:57 +0000

    added login.java

--- login.java
+++ login.java
@@ -1,2 +1,9 @@
-Adding first file in github repository
-Welcome to github
+public class test{
+        public static void main(String[]args){
+                int i;
+                for(i=0;i<=20;i++)
+                {
+                        println(i);
+
+                }
+
