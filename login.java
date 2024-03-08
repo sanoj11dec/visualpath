33d82db0cf4d54179284d029d214f9c4cfd5c6e5
Ubuntu <ubuntu@ip-172-31-28-208.ap-southeast-2.compute.internal>
2024-03-08 01:55:09 +0000

    updated i value to 50

--- login.java
+++ login.java
@@ -1,7 +1,7 @@
 public class test{
         public static void main(String[]args){
                 int i;
-                for(i=0;i<=20;i++)
+                for(i=0;i<=50;i++)
                 {
                         println(i);
 
